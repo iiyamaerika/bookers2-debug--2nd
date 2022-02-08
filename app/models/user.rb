7664c0292a998ef74d0617983cd7ff024e0aeab6
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-02-08 03:29:37 +0000

    [Add]検索機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_one_attached :profile_image
   has_many :books , dependent: :destroy
+  #いいね・コメント機能
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
-  
+
+  #フォロー・フォロワー機能
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  
+
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction,  length: {maximum: 50}
@@ -26,7 +28,7 @@ class User < ApplicationRecord
     end
       profile_image.variant(resize: size).processed
   end
-  
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
@@ -34,11 +36,21 @@ class User < ApplicationRecord
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
   end
-  
+
   def following?(user)
     followings.include?(user)
   end
-  
-  
-    
+
+  def self.search_for(content,method)
+    if method =='perfect'
+      User.where(name: content)
+    elsif method =='forward'
+      User.where('name LIKE?', content + "%")
+    elsif method =='backword'
+      User.where('name LIKE?', "%" + content )
+    else
+      elsifUser.where('name LIKE?', "%" + content + "%")
+    end
+  end
+
 end

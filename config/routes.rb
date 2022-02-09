c56a27ee8d49753f1dd997157c997a6b592b148d
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-02-09 15:56:46 +0000

    [Add] DM機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'chats/show'
   get 'relationships/followings'
   get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root :to => "homes#top"
   get "home/about" => "homes#about"
+  get "search" => "searches#search"
 
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
@@ -17,7 +19,7 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
 
-  get "search" => "searches#search"
+  resources :chats, only: [:show, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

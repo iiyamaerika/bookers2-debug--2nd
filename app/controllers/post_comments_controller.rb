b2eb8fc1fe504b9eeea3686d89667e2b98e95c9d
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-02-09 08:21:00 +0000

    [Add]コメント機能　非同期化

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,25 +1,29 @@
 class PostCommentsController < ApplicationController
-  
+  before_action :authenticate_user!
+
   def create
     @book = Book.find(params[:book_id])
-    @post_comment = current_user.post_comments.new(post_comment_params)
-    @post_comment.book_id = @book.id
+    @post_comment = PostComment.new(post_comment_params)
     @post_comment.user_id = current_user.id
+    @post_comment.book_id = @book.id
     @post_comment.save
-    redirect_to request.referer
+    unless @post_comment.save
+      render 'error'
+    end
+
   end
-  
+
   def destroy
-    book = Book.find(params[:book_id])
-    post_comment = book.post_comments.find(params[:id])
-    post_comment.destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @post_comment =  PostComment.find(params[:id])
+    @post_comment.destroy
+
   end
-  
+
   private
-  
+
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
-  
+
 end
